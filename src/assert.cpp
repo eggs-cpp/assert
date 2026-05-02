eb8c7f60040b3f2b0916ce5b23e19b82301353ee
Agustin Berge <agustinberge@gmail.com>
2026-05-02 16:30:06 +0200

    Improve diagnostic format

--- src/assert.cpp
+++ src/assert.cpp
@@ -13,7 +13,18 @@
 
 extern "C" [[noreturn]] void eggs_assert_failed(char const* message)
 {
-    auto const trace = std::stacktrace::current();
-    std::println(stderr, "Assertion `{}' failed.\n{}", message, trace);
+    std::println(stderr, "ASSERTION FAILURE:");
+
+    auto const trace = std::stacktrace::current(1);
+    if (!trace.empty()) {
+        auto const& top = trace[0];
+        std::println(stderr, "- file   : {}", top.source_file());
+        std::println(stderr, "- line   : {}", top.source_line());
+        std::println(stderr, "- context: {}", top.description());
+    }
+    std::println(stderr, "- message: {}\n", message);
+
+    std::println(stderr, "STACK TRACE:\n{}\n", trace);
+
     std::abort();
 }
